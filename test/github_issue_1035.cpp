9eb1c6d0a9a2437f25a0c58e39219545b54dfa73
Matt Borland <matt@mattborland.com>
2025-09-05 16:21:23 +0200

    Add test set from issue

---
+++ test/github_issue_1035.cpp
@@ -0,0 +1,28 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+// See: https://github.com/cppalliance/decimal/issues/1035
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+using namespace boost::decimal;
+using namespace boost::decimal::literals;
+
+int main()
+{
+    #ifdef BOOST_DECIMAL_NO_CONSTEVAL_DETECTION
+    return 0;
+    #else
+
+    fesetround(rounding_mode::fe_dec_downward);
+    BOOST_TEST_EQ("5e+50"_DF - "4e+40"_DF, "4.999999e+50"_DF);
+
+    fesetround(rounding_mode::fe_dec_upward);
+    BOOST_TEST_EQ("5e+50"_DF + "4e+40"_DF, "5.000001e+50"_DF);
+
+    return boost::report_errors();
+
+    #endif
+}
