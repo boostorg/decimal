38056e3c7935b4ed1d2d9390d5f5d678e1851d6a
Matt Borland <matt@mattborland.com>
2025-09-08 10:55:08 +0200

    Add the negative cases

--- test/github_issue_1035.cpp
+++ test/github_issue_1035.cpp
@@ -16,16 +16,18 @@ int main()
     BOOST_TEST_EQ(previously_inf, "500000e+90"_DF);
 
     #ifdef BOOST_DECIMAL_NO_CONSTEVAL_DETECTION
-    return boost::report_errors();;
+    return boost::report_errors();
     #else
 
     fesetround(rounding_mode::fe_dec_downward);
     BOOST_TEST_EQ("5e+50"_DF - "4e+40"_DF, "4.999999e+50"_DF);
     BOOST_TEST_EQ("5e+95"_DF - "4e-100"_DF, "4.999999e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DF + "4e-100"_DF, "-4.999999e+95"_DF);
 
     fesetround(rounding_mode::fe_dec_upward);
     BOOST_TEST_EQ("5e+50"_DF + "4e+40"_DF, "5.000001e+50"_DF);
     BOOST_TEST_EQ("5e+95"_DF + "4e-100"_DF, "5.000001e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DF - "4e-100"_DF, "-5.000001e+95"_DF);
 
     return boost::report_errors();
 
