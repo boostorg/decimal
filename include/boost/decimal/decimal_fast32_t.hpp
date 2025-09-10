aac4204fdbc109a96a65431b5e8e3d0814130d4d
Matt Borland <matt@mattborland.com>
2025-09-10 11:06:19 +0200

    Prepare for removal of defaulted template parameter

--- include/boost/decimal/decimal_fast32_t.hpp
+++ include/boost/decimal/decimal_fast32_t.hpp
@@ -895,7 +895,7 @@ constexpr auto operator*(const decimal_fast32_t lhs, const decimal_fast32_t rhs)
     auto res_exp {lhs.exponent_ + rhs.exponent_};
     res_exp -= res_sig_14_dig ? ten_pow_seven_exp_offset : ten_pow_six_exp_offset;
 
-    res_exp += detail::fenv_round(res_sig, isneg);
+    res_exp += detail::fenv_round<decimal_fast32_t>(res_sig, isneg);
 
     BOOST_DECIMAL_ASSERT(res_sig >= 1'000'000 || res_sig == 0U);
     BOOST_DECIMAL_ASSERT(res_exp <= 9'999'999 || res_sig == 0U);
@@ -1008,7 +1008,7 @@ constexpr auto div_impl(const decimal_fast32_t lhs, const decimal_fast32_t rhs, 
     // If we have 8 figures round it down to 7
     if (res_sig >= UINT64_C(10'000'000))
     {
-        res_exp += detail::fenv_round(res_sig, isneg);
+        res_exp += detail::fenv_round<decimal_fast32_t>(res_sig, isneg);
     }
 
     BOOST_DECIMAL_ASSERT(res_sig >= 1'000'000 || res_sig == 0U);
