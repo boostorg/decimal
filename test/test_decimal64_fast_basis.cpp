4ca018733af14065c3cab301862a3414a0f1105d
Matt Borland <matt@mattborland.com>
2025-09-22 13:36:46 +0200

    Attempt cover construction from double NAN

--- test/test_decimal64_fast_basis.cpp
+++ test/test_decimal64_fast_basis.cpp
@@ -144,7 +144,7 @@ void test_non_finite_values()
     BOOST_TEST(isinf(detail::check_non_finite(one, std::numeric_limits<decimal_fast64_t>::infinity() * dist(rng))));
     BOOST_TEST(isinf(detail::check_non_finite(std::numeric_limits<decimal_fast64_t>::infinity() * dist(rng), one)));
 
-    BOOST_TEST(isnan(decimal_fast64_t{std::numeric_limits<double>::quiet_NaN()}));
+    BOOST_TEST(isnan(decimal_fast64_t{std::numeric_limits<double>::quiet_NaN() * static_cast<double>(dist(rng))}));
 }
 
 #if !defined(__GNUC__) || (__GNUC__ != 7 && __GNUC__ != 8)
