2e0afb6b45e35a3159c9e621723a34bc6d09ee3d
Matt Borland <matt@mattborland.com>
2025-09-26 16:19:45 +0200

    Force runtime evaluation

--- test/github_issue_1035.cpp
+++ test/github_issue_1035.cpp
@@ -10,6 +10,7 @@
 
 #include <boost/core/lightweight_test.hpp>
 #include <iostream>
+#include <random>
 
 #ifdef BOOST_DECIMAL_USE_MODULE
 import boost.decimal;
@@ -20,7 +21,10 @@ using namespace boost::decimal::literals;
 
 int main()
 {
-    const auto previously_inf {"5e+95"_DF};
+    std::mt19937_64 rng(42);
+    std::uniform_int_distribution<int> dist(1, 1);
+
+    const auto previously_inf {"5e+95"_DF * dist(rng)};
     BOOST_TEST_EQ(previously_inf, "500000e+90"_DF);
 
     #ifdef BOOST_DECIMAL_NO_CONSTEVAL_DETECTION
@@ -28,16 +32,16 @@ int main()
     #else
 
     fesetround(rounding_mode::fe_dec_downward);
-    BOOST_TEST_EQ("5e+50"_DF - "4e+40"_DF, "4.999999e+50"_DF);
-    BOOST_TEST_EQ("5e+95"_DF - "4e-100"_DF, "4.999999e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DF + "4e-100"_DF, "-4.999999e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DL + "4e-100"_DL, "-4.999999999999999999999999999999999e+95"_DL);
+    BOOST_TEST_EQ("5e+50"_DF * dist(rng) - "4e+40"_DF, "4.999999e+50"_DF);
+    BOOST_TEST_EQ("5e+95"_DF * dist(rng)  - "4e-100"_DF, "4.999999e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DF * dist(rng)  + "4e-100"_DF, "-4.999999e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DL * dist(rng)  + "4e-100"_DL, "-4.999999999999999999999999999999999e+95"_DL);
 
     fesetround(rounding_mode::fe_dec_upward);
-    BOOST_TEST_EQ("5e+50"_DF + "4e+40"_DF, "5.000001e+50"_DF);
-    BOOST_TEST_EQ("5e+95"_DF + "4e-100"_DF, "5.000001e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DF - "4e-100"_DF, "-5.000001e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DL - "4e-100"_DL, "-5.000000000000000000000000000000001e+95"_DL);
+    BOOST_TEST_EQ("5e+50"_DF * dist(rng) + "4e+40"_DF, "5.000001e+50"_DF);
+    BOOST_TEST_EQ("5e+95"_DF * dist(rng)  + "4e-100"_DF, "5.000001e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DF * dist(rng)  - "4e-100"_DF, "-5.000001e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DL * dist(rng)  - "4e-100"_DL, "-5.000000000000000000000000000000001e+95"_DL);
 
     return boost::report_errors();
 
