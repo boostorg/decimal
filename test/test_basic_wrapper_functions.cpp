0334f692073c2e42fcf354c04410c570a70df189
Matt Borland <matt@mattborland.com>
2025-09-30 14:25:48 +0200

    Add testing of comparison operators

--- test/test_basic_wrapper_functions.cpp
+++ test/test_basic_wrapper_functions.cpp
@@ -44,11 +44,51 @@ void test_constructor()
     }
 }
 
+template <typename T>
+void test_comparisons()
+{
+    using software_type = std::conditional_t<std::is_same<T, builtin_decimal32_t>::value, decimal32_t,
+                            std::conditional_t<std::is_same<T, builtin_decimal64_t>::value, decimal64_t, decimal128_t>>;
+
+    std::uniform_int_distribution<long long> coeff_dist {LLONG_MIN, LLONG_MAX};
+    std::uniform_int_distribution<int> exp_dist {-20, 20};
+
+    for (std::size_t i = 0; i < N; ++i)
+    {
+        const auto lhs_coeff {coeff_dist(rng)};
+        const auto rhs_coeff {coeff_dist(rng)};
+
+        const auto lhs_exp {exp_dist(rng)};
+        const auto rhs_exp {exp_dist(rng)};
+
+        const software_type software_lhs {lhs_coeff, lhs_exp};
+        const software_type software_rhs {rhs_coeff, rhs_exp};
+
+        const T hardware_lhs {lhs_coeff, lhs_exp};
+        const T hardware_rhs {rhs_coeff, rhs_exp};
+
+        BOOST_TEST((software_lhs == software_rhs) == (hardware_lhs == hardware_rhs));
+        BOOST_TEST((software_lhs != software_rhs) == (hardware_lhs != hardware_rhs));
+        BOOST_TEST((software_lhs < software_rhs) == (hardware_lhs < hardware_rhs));
+        BOOST_TEST((software_lhs <= software_rhs) == (hardware_lhs <= hardware_rhs));
+        BOOST_TEST((software_lhs > software_rhs) == (hardware_lhs > hardware_rhs));
+        BOOST_TEST((software_lhs >= software_rhs) == (hardware_lhs >= hardware_rhs));
+
+        #ifdef BOOST_DECIMAL_HAS_SPACESHIP_OPERATOR
+        BOOST_TEST((software_lhs <=> software_rhs) == (hardware_lhs <=> hardware_rhs));
+        #endif
+    }
+}
+
 int main()
 {
     test_constructor<builtin_decimal32_t>();
     test_constructor<builtin_decimal64_t>();
     test_constructor<builtin_decimal128_t>();
+
+    test_comparisons<builtin_decimal32_t>();
+    test_comparisons<builtin_decimal64_t>();
+    test_comparisons<builtin_decimal128_t>();
 
     return boost::report_errors();
 }
