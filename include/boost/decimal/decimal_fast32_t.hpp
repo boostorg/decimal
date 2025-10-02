9cda3a78354e1e3ee4d3ee8f33e8899596a5633a
Matt Borland <matt@mattborland.com>
2025-10-02 10:48:41 +0200

    Remove dependency due to masks

--- include/boost/decimal/decimal_fast32_t.hpp
+++ include/boost/decimal/decimal_fast32_t.hpp
@@ -5,7 +5,6 @@
 #ifndef BOOST_DECIMAL_decimal_fast32_t_HPP
 #define BOOST_DECIMAL_decimal_fast32_t_HPP
 
-#include <boost/decimal/decimal32_t.hpp>
 #include <boost/decimal/detail/apply_sign.hpp>
 #include <boost/decimal/detail/type_traits.hpp>
 #include <boost/decimal/detail/integer_search_trees.hpp>
