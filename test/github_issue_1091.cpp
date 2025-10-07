374328ea0d55de8b1fa2cef34b487f88d8ec7757
Matt Borland <matt@mattborland.com>
2025-10-07 13:43:21 +0200

    Add reproducer test set

---
+++ test/github_issue_1091.cpp
@@ -0,0 +1,34 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+// See: https://github.com/cppalliance/decimal/issues/1091
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+using namespace boost::decimal;
+
+template <typename T>
+void test()
+{
+    constexpr T one {1u, 0};
+    T low {std::numeric_limits<T>::lowest()};
+    BOOST_TEST(!isinf(low));
+
+    low *= 2;
+
+    BOOST_TEST(low < one);
+    BOOST_TEST(one > low);
+    BOOST_TEST(isinf(low));
+}
+
+int main()
+{
+    test<decimal32_t>();
+    test<decimal64_t>();
+    test<decimal128_t>();
+
+    return boost::report_errors();
+}
+
