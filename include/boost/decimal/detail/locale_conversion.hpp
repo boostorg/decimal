c9b96ea53e173ba43d99fb00d8ca3105237c6fbd
Matt Borland <matt@mattborland.com>
2025-10-16 13:53:09 +0200

    Fix handling of thousands seperators

--- include/boost/decimal/detail/locale_conversion.hpp
+++ include/boost/decimal/detail/locale_conversion.hpp
@@ -17,10 +17,55 @@ namespace detail {
 
 inline void convert_string_to_c_locale(char* buffer) noexcept
 {
-    const auto locale_decimal_point = *std::localeconv()->decimal_point;
+    const auto* lconv {std::localeconv()};
+    const auto locale_decimal_point {*lconv->decimal_point};
+    auto locale_thousands_sep {*lconv->thousands_sep};
+    if (locale_thousands_sep == -30)
+    {
+        // Locales like french use a space, but thousands_sep does not give a space character
+        locale_thousands_sep = ' ';
+    }
+    const bool has_grouping {lconv->grouping && lconv->grouping[0] > 0};
+
+    // Remove thousands separator if it exists and grouping is enabled
+    if (has_grouping && locale_thousands_sep != '\0')
+    {
+        // Find the decimal point first to know where the integer part ends
+        const auto decimal_pos {std::strchr(buffer, static_cast<int>(locale_decimal_point))};
+        const auto int_end {decimal_pos ? decimal_pos : (buffer + std::strlen(buffer))};
+
+        // Find the start of the number to include skipping sign
+        auto start {buffer};
+        if (*start == '-' || *start == '+')
+        {
+            ++start;
+        }
+
+        // Only remove thousands separators from the integer part
+        auto read {start};
+        auto write {start};
+
+        while (read < int_end)
+        {
+            const auto ch = *read;
+            if (ch != locale_thousands_sep)
+            {
+                *write++ = *read;
+            }
+            ++read;
+        }
+
+        // Copy the rest of the string (decimal point and fractional part)
+        while (*read != '\0')
+        {
+            *write++ = *read++;
+        }
+        *write = '\0';
+    }
+
     if (locale_decimal_point != '.')
     {
-        auto p = std::strchr(buffer, static_cast<int>(locale_decimal_point));
+        const auto p {std::strchr(buffer, static_cast<int>(locale_decimal_point))};
         if (p != nullptr)
         {
             *p = '.';
