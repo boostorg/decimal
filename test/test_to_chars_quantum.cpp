a83fe237dc4d137bf5ff22ad01082147c9537555
Matt Borland <matt@mattborland.com>
2025-10-17 15:33:28 +0200

    Begin adding test set

---
+++ test/test_to_chars_quantum.cpp
@@ -0,0 +1,48 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <array>
+
+using namespace boost::decimal;
+
+template <typename T>
+void test_to_chars_scientific()
+{
+    const std::array<T, 7> decimals = {
+        T{3, 2},
+        T{30, 1},
+        T{300, 0},
+        T{3000, -1},
+        T{30000, -2},
+        T{300000, -3},
+        T{3000000, -4},
+    };
+
+    const std::array<const char*, 7> strings = {
+        "3e+02",
+        "3.0e+02",
+        "3.00e+02",
+        "3.000e+02",
+        "3.0000e+02",
+        "3.00000e+02",
+        "3.000000e+02",
+    };
+
+    for (std::size_t i {}; i < decimals.size(); ++i)
+    {
+        char buffer[64] {};
+        const auto r {to_chars(buffer, buffer + sizeof(buffer), decimals[i], chars_format::scientific, quantum_preservation::on)};
+        BOOST_TEST(r);
+        BOOST_TEST_CSTR_EQ(buffer, strings[i]);
+    }
+}
+
+int main()
+{
+    test_to_chars_scientific<decimal32_t>();
+
+    return boost::report_errors();
+}
