5fb7d29d5647e1dfb6f65437c854c77a9e36de52
Matt Borland <matt@mattborland.com>
2025-10-20 15:27:38 +0200

    Fix construction of 128-bit subnormal values

--- include/boost/decimal/decimal128_t.hpp
+++ include/boost/decimal/decimal128_t.hpp
@@ -745,7 +745,7 @@ constexpr decimal128_t::decimal128_t(T1 coeff, T2 exp, bool sign) noexcept
     auto biased_exp {static_cast<int>(exp + detail::bias_v<decimal128_t>)};
     BOOST_DECIMAL_IF_CONSTEXPR (sizeof(T1) >= sizeof(significand_type))
     {
-        if (coeff > detail::d128_max_significand_value || biased_exp < 0)
+        if (coeff > detail::d128_max_significand_value || biased_exp < -(detail::precision_v<decimal128_t> - 1))
         {
             coeff_digits = detail::coefficient_rounding<decimal128_t>(coeff, exp, biased_exp, sign, detail::num_digits(coeff));
         }
@@ -787,6 +787,19 @@ constexpr decimal128_t::decimal128_t(T1 coeff, T2 exp, bool sign) noexcept
             exp -= digit_delta;
             reduced_coeff *= detail::pow10(static_cast<significand_type>(digit_delta));
             *this = decimal128_t(reduced_coeff, exp, sign);
+        }
+        else if (coeff_digits + biased_exp <= detail::precision_v<decimal128_t>)
+        {
+            // Handle the case of sub-normals that don't need further rounding
+            bits_.high = sign ? detail::d128_sign_mask : UINT64_C(0); // Reset the sign bit
+            const auto zeros {detail::remove_trailing_zeros(reduced_coeff)};
+            biased_exp += zeros.number_of_removed_zeros;
+            reduced_coeff = zeros.trimmed_number;
+            if (biased_exp > 0)
+            {
+                reduced_coeff *= detail::pow10(static_cast<significand_type>(biased_exp));
+            }
+            bits_ |= reduced_coeff;
         }
         else if (digit_delta < 0 && coeff_digits - digit_delta <= detail::precision_v<decimal128_t>)
         {
