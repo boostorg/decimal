7c3e5c77b894d9c3d1ae963a50f7e4745ff59e06
Matt Borland <matt@mattborland.com>
2025-11-05 13:51:11 +0100

    Add testing of other decimal types

--- test/test_from_chars_nan_payloads.cpp
+++ test/test_from_chars_nan_payloads.cpp
@@ -5,18 +5,18 @@
 #include <boost/decimal.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <array>
+#include <string>
 
 using namespace boost::decimal;
 
 template <typename T>
 void test_signaling()
 {
-    const std::array<const char*, 6> lhs_values { "nan(snan)", "SNAN", "SNAN(42)", "SNAN42", "sNaN400JUNK", "nan(snan42)" };
-    const std::array<const char*, 6> rhs_values { "nan(snan)", "SNAN", "SNAN(43)", "SNAN43", "SnAn410JUNK", "nan(snan4000)" };
+    const std::array<std::string, 6> lhs_values { "nan(snan)", "SNAN", "SNAN(42)", "SNAN42", "sNaN400JUNK", "nan(snan42)" };
+    const std::array<std::string, 6> rhs_values { "nan(snan)", "SNAN", "SNAN(43)", "SNAN43", "SnAn410JUNK", "nan(snan4000)" };
 
     for (std::size_t i {}; i < lhs_values.size(); ++i)
     {
-        std::cerr << "I: " << i << std::endl;
         const T lhs {lhs_values[i]};
         const T rhs {rhs_values[i]};
 
@@ -37,6 +37,12 @@ void test_signaling()
 int main()
 {
     test_signaling<decimal32_t>();
+    test_signaling<decimal64_t>();
+    test_signaling<decimal128_t>();
+
+    test_signaling<decimal_fast32_t>();
+    test_signaling<decimal_fast64_t>();
+    test_signaling<decimal_fast128_t>();
 
     return boost::report_errors();
 }
