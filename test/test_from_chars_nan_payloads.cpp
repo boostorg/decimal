22dfffd485f40684f1806dd974123293ab0ad9af
Matt Borland <matt@mattborland.com>
2025-11-05 13:55:33 +0100

    Add qnan testing and change some capitalization

--- test/test_from_chars_nan_payloads.cpp
+++ test/test_from_chars_nan_payloads.cpp
@@ -12,8 +12,8 @@ using namespace boost::decimal;
 template <typename T>
 void test_signaling()
 {
-    const std::array<std::string, 6> lhs_values { "nan(snan)", "SNAN", "SNAN(42)", "SNAN42", "sNaN400JUNK", "nan(snan42)" };
-    const std::array<std::string, 6> rhs_values { "nan(snan)", "SNAN", "SNAN(43)", "SNAN43", "SnAn410JUNK", "nan(snan4000)" };
+    const std::array<std::string, 6> lhs_values { "NAN(SNAN)", "SNAN", "SNAN(42)", "SNAN42", "sNaN400JUNK", "NAN(SNAN42)" };
+    const std::array<std::string, 6> rhs_values { "nan(snan)", "snan", "snan(43)", "snan43", "SnAn410JUNK", "nan(snan4000)" };
 
     for (std::size_t i {}; i < lhs_values.size(); ++i)
     {
@@ -34,6 +34,31 @@ void test_signaling()
     }
 }
 
+template <typename T>
+void test_quiet()
+{
+    const std::array<std::string, 6> lhs_values { "nan(IND)", "NAN", "NAN(42)", "NAN42", "NaN400JUNK", "nan(IND4200)" };
+    const std::array<std::string, 6> rhs_values { "nan(ind)", "nan", "nan(43)", "nan43", "nAn410junk", "nan(ind4000)" };
+
+    for (std::size_t i {}; i < lhs_values.size(); ++i)
+    {
+        const T lhs {lhs_values[i]};
+        const T rhs {rhs_values[i]};
+
+        BOOST_TEST(isnan(lhs));
+        BOOST_TEST(isnan(rhs));
+
+        BOOST_TEST(!issignaling(lhs));
+        BOOST_TEST(!issignaling(rhs));
+
+        if (i >= 2)
+        {
+            // A nan with a higher payload compares higher
+            BOOST_TEST(comparetotal(lhs, rhs));
+        }
+    }
+}
+
 int main()
 {
     test_signaling<decimal32_t>();
@@ -43,6 +68,14 @@ int main()
     test_signaling<decimal_fast32_t>();
     test_signaling<decimal_fast64_t>();
     test_signaling<decimal_fast128_t>();
+
+    test_quiet<decimal32_t>();
+    test_quiet<decimal64_t>();
+    test_quiet<decimal128_t>();
+
+    test_quiet<decimal_fast32_t>();
+    test_quiet<decimal_fast64_t>();
+    test_quiet<decimal_fast128_t>();
 
     return boost::report_errors();
 }
