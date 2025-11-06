5ffac74da77cad67490a322326669b9080244fa9
Matt Borland <matt@mattborland.com>
2025-11-06 15:54:37 +0100

    Remove bit_string function

--- test/test_decimal128_basis.cpp
+++ test/test_decimal128_basis.cpp
@@ -14,61 +14,6 @@
 
 using namespace boost::decimal;
 
-#if BOOST_DECIMAL_ENDIAN_LITTLE_BYTE && defined(BOOST_DECIMAL_HAS_INT128)
-void test_binary_constructor()
-{
-    decimal128_t one(0b1, -6175);
-    // 0 for sign
-    // 00000 for combination field
-    // 00000000 for exp
-    // 1 for significand
-    BOOST_TEST_EQ(bit_string(one), "4000000000001");
-
-    decimal128_t neg_one(-0b1, -6175);
-    // 1 for sign
-    // 00000 for combination field
-    // 00000000 for exp
-    // 1 for significand
-    BOOST_TEST_EQ(bit_string(neg_one), "80004000000000001");
-
-    decimal128_t two(0b10, -6175);
-    // 0 for sign
-    // 00000 for combination field
-    // 00000000 for exp
-    // 2 for significand
-    BOOST_TEST_EQ(bit_string(two), "4000000000002");
-
-    decimal128_t three(0b11, -6175);
-    // 0 for sign
-    // 00000 for combination field
-    // 00000000 for exp
-    // 3 for significand
-    BOOST_TEST_EQ(bit_string(three), "4000000000003");
-
-    decimal128_t big(UINT64_MAX, -6175);
-    // 0 for sign
-    // 00000 for combination field
-    // 00000000 for exp
-    // FFFFFFFFFFFFFFFF for significand
-    BOOST_TEST_EQ(bit_string(big), "400000000000ffffffffffffffff");
-
-    decimal128_t onee1(0b1, -6174);
-    // 0 for sign
-    // 00000 for combination field
-    // 00000001 for exp
-    // 1 for significand
-    BOOST_TEST_EQ(bit_string(onee1), "8000000000001");
-}
-
-#else
-
-void test_binary_constructor()
-{
-    //nothing
-}
-
-#endif
-
 void test_non_finite_values()
 {
     const decimal128_t one(0b1, 0);
@@ -123,7 +68,6 @@ void test_float_constructor()
 
 int main()
 {
-    test_binary_constructor();
     test_non_finite_values();
 
     test_float_constructor();
