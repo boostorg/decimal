d457c944c8d92a6c46b371fa96599189a130e9d2
Matt Borland <matt@mattborland.com>
2025-11-13 15:27:58 +0100

    Improve comments and code clarity

--- examples/numerical_parsing.cpp
+++ examples/numerical_parsing.cpp
@@ -35,6 +35,10 @@ decimal32_t parse_opening_price<decimal32_t>(const std::string& line)
     decimal32_t result;
     const auto r = from_chars(line, result);
 
+    // If we have a parse failure throw std::invalid_argument if the environment supports it,
+    // using std::invalid_argument which is the same thing thrown by std::stof in the float case
+    //
+    // If we are in a no throw environment returning a qNaN will poison our results as well
     if (!r)
     {
         // LCOV_EXCL_START
@@ -47,7 +51,7 @@ decimal32_t parse_opening_price<decimal32_t>(const std::string& line)
 }
 
 template <typename T>
-auto parse_csv_line(const std::string& line) -> T
+T parse_csv_line(const std::string& line)
 {
     std::stringstream ss(line);
     std::string token;
@@ -61,6 +65,10 @@ auto parse_csv_line(const std::string& line) -> T
 
 int main()
 {
+    // We have a CSV file containing one years worth of daily stock data for AAPL
+    // Here we will show the differences that arise (however small)
+    // between parsing with float and decimal32_t
+
     // Open and read the CSV file
     std::ifstream file(boost::decimal::where_file("AAPL.csv"));
     std::string line;
@@ -71,19 +79,28 @@ int main()
     std::vector<decimal32_t> decimal_opening_prices;
     std::vector<float> float_opening_prices;
 
+    // Parse each line once into decimal32_t and once into a float
     while (std::getline(file, line))
     {
         decimal_opening_prices.emplace_back(parse_csv_line<decimal32_t>(line));
         float_opening_prices.emplace_back(parse_csv_line<float>(line));
     }
 
-    const std::string ms_result {"52151.99"};
-    const auto decimal_sum {std::accumulate(decimal_opening_prices.begin(), decimal_opening_prices.end(), decimal32_t{0})};
-    const auto float_sum {std::accumulate(float_opening_prices.begin(), float_opening_prices.end(), float{0})};
+    // Use std::accumulate to get the sum of all the pricing information in the array
+    // This will be used to compare the total value parsed
+    const auto decimal_sum {std::accumulate(decimal_opening_prices.begin(),
+        decimal_opening_prices.end(), decimal32_t{0})};
+    
+    const auto float_sum {std::accumulate(float_opening_prices.begin(),
+        float_opening_prices.end(), float{0})};
+
+    // This is the reference value that was found using the sum command of the CSV
+    // inside Microsoft Excel
+    const std::string ms_excel_result {"52151.99"};
 
     std::cout << std::setprecision(std::numeric_limits<float>::digits10 + 1)
               << "Number of data points: " << decimal_opening_prices.size() << '\n'
-              << "    Sum from MS Excel: " << ms_result << '\n'
+              << "    Sum from MS Excel: " << ms_excel_result << '\n'
               << "Sum using decimal32_t: " << decimal_sum << '\n'
               << "      Sum using float: " << float_sum << std::endl;
 }
