2c2d1706785bb0b0bc7c267b9c662cbffe29b84e
Matt Borland <matt@mattborland.com>
2025-12-10 16:20:17 +0100

    Always print a minimum of 2 digits of exponent

--- extra/detail/generate_string.py
+++ extra/detail/generate_string.py
@@ -4,16 +4,18 @@
 
 def generate_string(significand, exp, sign):
     if significand == 0:
-        result = "0.0e+0"
+        result = "0e+00"
     else:
         sig_str = str(significand)
         n_digits = len(sig_str)
+
         if n_digits == 1:
             normalized_str = sig_str
             total_exp = exp
         else:
             normalized_str = sig_str[0] + '.' + sig_str[1:]
             total_exp = exp + n_digits - 1
-        result = f"{'-' if sign else ''}{normalized_str}e{total_exp:+}"
+
+        result = f"{'-' if sign else ''}{normalized_str}e{total_exp:+03d}"
 
     return result
