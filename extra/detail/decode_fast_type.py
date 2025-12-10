32c8cace1a6be6f1a4b1535525f3489a107a20b1
Matt Borland <matt@mattborland.com>
2025-12-10 15:10:53 +0100

    Add proper handling of non-finite decimal_fast32_t values

--- extra/detail/decode_fast_type.py
+++ extra/detail/decode_fast_type.py
@@ -9,5 +9,29 @@ sys.path.insert(0, os.path.dirname(__file__))
 from generate_string import generate_string
 
 def decode_decimal_fast32(significand, exp, sign):
-    return generate_string(significand, exp, sign)
 
+    # See values of non-finite masks in decimal_fast32_t.hpp
+    if significand >= 536870912:
+        isnan = False
+
+        if significand == 536870912:
+            result = "-INF" if sign else "INF"
+        elif significand >= 3758096384:
+            result = "-SNAN" if sign else "SNAN"
+            significand ^= 3758096384
+            isnan = True
+        elif significand >= 1610612736:
+            result = "-QNAN" if sign else "QNAN"
+            significand ^= 1610612736
+            isnan = True
+        else:
+            raise ValueError("Unknown Finite Value")
+
+        if isnan and significand > 0:
+            result += '(' + str(significand) + ')'
+
+    else:
+        exp -= 101 # Bias value
+        result = generate_string(significand, exp, sign)
+
+    return result
