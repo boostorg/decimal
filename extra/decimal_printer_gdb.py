eb197c47622cf9431326989f0e6d0b639cfe68ad
Matt Borland <matt@mattborland.com>
2025-12-15 14:31:14 -0500

    Fix missing detail folder

--- extra/decimal_printer_gdb.py
+++ extra/decimal_printer_gdb.py
@@ -2,6 +2,10 @@
 # Distributed under the Boost Software License, Version 1.0.
 # https://www.boost.org/LICENSE_1_0.txt
 
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from detail.decode_ieee_type import decode_decimal32
 from detail.decode_ieee_type import decode_decimal64
 from detail.decode_ieee_type import decode_decimal128
