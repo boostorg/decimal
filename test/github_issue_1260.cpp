d4190857612e54dcc7118cff1df2d670f0b435d8
Matt Borland <matt@mattborland.com>
2025-12-17 16:52:04 -0500

    Add additional test case

--- test/github_issue_1260.cpp
+++ test/github_issue_1260.cpp
@@ -21,10 +21,26 @@ void test()
     BOOST_TEST_EQ(sub_val, res);
 }
 
+template <typename T>
+void test2()
+{
+    const T lhs {"1E34"};
+    const T rhs {"-0.501"};
+    const T res {"9999999999999999999999999999999999"};
+
+    const T add_val {lhs + rhs};
+    BOOST_TEST_EQ(add_val, res);
+
+    const T sub_val {lhs - boost::decimal::abs(rhs)};
+    BOOST_TEST_EQ(sub_val, res);
+}
+
 int main()
 {
     test<boost::decimal::decimal128_t>();
     test<boost::decimal::decimal_fast128_t>();
 
+    test2<boost::decimal::decimal128_t>();
+    test2<boost::decimal::decimal_fast128_t>();
     return boost::report_errors();
 }
