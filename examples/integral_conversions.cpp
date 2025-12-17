3e210dabed0ef9193ddf0ebcf4fd88a75e60296d
Matt Borland <matt@mattborland.com>
2025-12-17 12:05:43 -0500

    Fix MSVC warning

--- examples/integral_conversions.cpp
+++ examples/integral_conversions.cpp
@@ -55,25 +55,24 @@ int main()
     // Constructing a decimal value from an integer is lossless until
     // the number of digits in the integer exceeds the precision of the decimal type
 
-    if (std::numeric_limits<decimal64_t>::digits10 >= std::numeric_limits<std::uint32_t>::digits10)
-    {
-        std::cout << "Conversions will be lossless\n";
+    std::cout << "Conversions will be lossless\n"
+              << "  decimal64_t digits10: " << std::numeric_limits<decimal64_t>::digits10 << "\n"
+              << "std::uint32_t digits10: " << std::numeric_limits<std::uint32_t>::digits10 << "\n";
 
-        constexpr decimal64_t decimal_from_max {std::numeric_limits<std::uint32_t>::max()};
-        std::cout << "   std::uint32_t max: " << std::numeric_limits<std::uint32_t>::max() << "\n"
-                  << "decimal64_t from max: " << decimal_from_max << "\n\n";
-    }
+    constexpr decimal64_t decimal_from_u32_max {std::numeric_limits<std::uint32_t>::max()};
+    std::cout << "   std::uint32_t max: " << std::numeric_limits<std::uint32_t>::max() << "\n"
+              << "decimal64_t from max: " << decimal_from_u32_max << "\n\n";
 
-    if (std::numeric_limits<decimal32_t>::digits10 < std::numeric_limits<std::uint64_t>::digits10)
-    {
-        // In the construction of lossy values the rounding will be handled according to
-        // the current global rounding mode.
+    // In the construction of lossy values the rounding will be handled according to
+    // the current global rounding mode.
 
-        std::cout << "Conversions will be lossy\n";
-        constexpr decimal32_t decimal_from_max {std::numeric_limits<std::uint64_t>::max()};
-        std::cout << "   std::uint64_t max: " << std::numeric_limits<std::uint64_t>::max() << "\n"
-                  << "decimal32_t from max: " << decimal_from_max << '\n';
-    }
+    std::cout << "Conversions will be lossy\n"
+          << "  decimal32_t digits10: " << std::numeric_limits<decimal32_t>::digits10 << "\n"
+          << "std::uint64_t digits10: " << std::numeric_limits<std::uint64_t>::digits10 << "\n";
+
+    constexpr decimal32_t decimal_from_u64_max {std::numeric_limits<std::uint64_t>::max()};
+    std::cout << "   std::uint64_t max: " << std::numeric_limits<std::uint64_t>::max() << "\n"
+              << "decimal32_t from max: " << decimal_from_u64_max << '\n';
 
     return 0;
 }
