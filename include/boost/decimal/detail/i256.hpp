a20dbc2d1c921d3330831b2ed587246e211da7de
Matt Borland <matt@mattborland.com>
2026-01-12 10:28:01 -0500

    Explicitly construct u256 for u128

--- include/boost/decimal/detail/i256.hpp
+++ include/boost/decimal/detail/i256.hpp
@@ -135,7 +135,7 @@ constexpr bool i256_sub_impl(const u256& a, const u256& b, u256& result) noexcep
 
 constexpr bool i256_sub(const int128::uint128_t& a, const int128::uint128_t& b, u256& result) noexcept
 {
-    return impl::i256_sub_impl(a, b, result);
+    return impl::i256_sub_impl(u256{a}, u256{b}, result);
 }
 
 #if !defined(BOOST_DECIMAL_NO_CONSTEVAL_DETECTION) && defined(BOOST_DECIMAL_ADD_CARRY)
@@ -181,7 +181,8 @@ constexpr bool i256_sub(const u256& a, const u256& b, u256& res) noexcept
 
 namespace impl {
 
-inline bool sub_borrow_u64_intrin(const bool borrow_in, const std::uint64_t a, const std::uint64_t b, std::uint64_t& diff) noexcept {
+inline bool sub_borrow_u64_intrin(const bool borrow_in, const std::uint64_t a, const std::uint64_t b, std::uint64_t& diff) noexcept
+{
     unsigned long long res;
     auto c = __builtin_usubll_overflow(a, b, &res);
     c |= __builtin_usubll_overflow(res, static_cast<unsigned long long>(borrow_in), &res);
