ead15473ca69c580ceb8ec2bd97909806dd90bcc
Matt Borland <matt@mattborland.com>
2026-01-14 14:27:48 -0500

    Add similar fmin tests

--- test/github_issue_1299.cpp
+++ test/github_issue_1299.cpp
@@ -24,7 +24,7 @@ enum class result_sign_t
 };
 
 template <typename T>
-void test_value(const T lhs, const T rhs, const result_t result, const result_sign_t sign = result_sign_t::positive, const int payload_value = 0)
+void test_fmax_value(const T lhs, const T rhs, const result_t result, const result_sign_t sign = result_sign_t::positive, const int payload_value = 0)
 {
     const auto res {fmax(lhs, rhs)};
 
@@ -57,42 +57,79 @@ void test_value(const T lhs, const T rhs, const result_t result, const result_si
     }
 }
 
-#ifdef _MSC_VER
-# pragma warning(push)
-# pragma warning(disable: 4127) // Conditional expression for BOOST_IF_CONSTEXPR will be constant in not C++17
-#endif
+template <typename T>
+void test_fmin_value(const T lhs, const T rhs, const result_t result, const result_sign_t sign = result_sign_t::positive, const int payload_value = 0)
+{
+    const auto res {fmin(lhs, rhs)};
+
+    switch (result)
+    {
+        case result_t::lhs:
+            BOOST_TEST_EQ(lhs, res);
+            break;
+
+        case result_t::rhs:
+            BOOST_TEST_EQ(rhs, res);
+            break;
+
+        case result_t::qnan:
+            BOOST_TEST(isnan(res));
+            BOOST_TEST(!issignaling(res));
+
+            if (sign == result_sign_t::negative)
+            {
+                BOOST_TEST(signbit(res));
+            }
+
+            if (payload_value)
+            {
+                const auto payload {read_payload(res)};
+                BOOST_TEST_EQ(static_cast<decltype(payload)>(payload_value), payload);
+            }
+
+            break;
+    }
+}
 
 template <typename T>
 void test_driver()
 {
-    test_value(T{5}, T{42}, result_t::rhs);
-    test_value(std::numeric_limits<T>::infinity(), std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
-    test_value(-std::numeric_limits<T>::infinity(), std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
-    test_value(T{5}, std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
+    test_fmax_value(T{5}, T{42}, result_t::rhs);
+    test_fmax_value(std::numeric_limits<T>::infinity(), std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
+    test_fmax_value(-std::numeric_limits<T>::infinity(), std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
+    test_fmax_value(T{5}, std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
 
     // Any operation on SNAN is invalid and returns QNAN with the same payload (as applicable)
-    test_value(std::numeric_limits<T>::signaling_NaN(), T{5}, result_t::qnan);
-    test_value(std::numeric_limits<T>::quiet_NaN(), std::numeric_limits<T>::signaling_NaN(), result_t::qnan);
-    test_value(std::numeric_limits<T>::signaling_NaN(), std::numeric_limits<T>::quiet_NaN(), result_t::qnan);
+    test_fmax_value(std::numeric_limits<T>::signaling_NaN(), T{5}, result_t::qnan);
+    test_fmax_value(std::numeric_limits<T>::quiet_NaN(), std::numeric_limits<T>::signaling_NaN(), result_t::qnan);
+    test_fmax_value(std::numeric_limits<T>::signaling_NaN(), std::numeric_limits<T>::quiet_NaN(), result_t::qnan);
 
     T snan_payload {"-sNaN97"};
-    test_value(snan_payload, std::numeric_limits<T>::quiet_NaN(), result_t::qnan, result_sign_t::negative, 97);
-    test_value(std::numeric_limits<T>::quiet_NaN(), snan_payload, result_t::qnan, result_sign_t::negative, 97);
+    test_fmax_value(snan_payload, std::numeric_limits<T>::quiet_NaN(), result_t::qnan, result_sign_t::negative, 97);
+    test_fmax_value(std::numeric_limits<T>::quiet_NaN(), snan_payload, result_t::qnan, result_sign_t::negative, 97);
 
     snan_payload = -snan_payload;
     T qnan_payload {"NaN100"};
-    test_value(snan_payload, qnan_payload, result_t::qnan, result_sign_t::positive, 97);
+    test_fmax_value(snan_payload, qnan_payload, result_t::qnan, result_sign_t::positive, 97);
 
-    BOOST_DECIMAL_IF_CONSTEXPR (std::numeric_limits<T>::digits < 11)
-    {
-        snan_payload = T{"-sNaN12345678901"};
-        test_value(snan_payload, qnan_payload, result_t::qnan, result_sign_t::positive, 5678901);
-    }
+    test_fmin_value(T{5}, T{42}, result_t::lhs);
+    test_fmin_value(std::numeric_limits<T>::infinity(), std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
+    test_fmin_value(-std::numeric_limits<T>::infinity(), std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
+    test_fmin_value(T{5}, std::numeric_limits<T>::quiet_NaN(), result_t::lhs);
+
+    // Any operation on SNAN is invalid and returns QNAN with the same payload (as applicable)
+    test_fmin_value(std::numeric_limits<T>::signaling_NaN(), T{5}, result_t::qnan);
+    test_fmin_value(std::numeric_limits<T>::quiet_NaN(), std::numeric_limits<T>::signaling_NaN(), result_t::qnan);
+    test_fmin_value(std::numeric_limits<T>::signaling_NaN(), std::numeric_limits<T>::quiet_NaN(), result_t::qnan);
+
+    snan_payload = T{"-sNaN97"};
+    test_fmin_value(snan_payload, std::numeric_limits<T>::quiet_NaN(), result_t::qnan, result_sign_t::negative, 97);
+    test_fmin_value(std::numeric_limits<T>::quiet_NaN(), snan_payload, result_t::qnan, result_sign_t::negative, 97);
+
+    snan_payload = -snan_payload;
+    qnan_payload = T{"NaN100"};
+    test_fmin_value(snan_payload, qnan_payload, result_t::qnan, result_sign_t::positive, 97);
 }
-
-#ifdef _MSC_VER
-# pragma warning(pop)
-#endif
 
 int main()
 {
