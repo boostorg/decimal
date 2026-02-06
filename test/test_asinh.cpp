ecd408c6848fe345fdaaf86074f2a4f5baed55a3
ckormanyos <ckormanyos@yahoo.com>
2026-02-06 13:04:34 +0100

    Test tuning and improvement

--- test/test_asinh.cpp
+++ test/test_asinh.cpp
@@ -1,5 +1,5 @@
 // Copyright 2023 Matt Borland
-// Copyright 2023 Christopher Kormanyos
+// Copyright 2023 - 2026 Christopher Kormanyos
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
@@ -236,7 +236,8 @@ auto main() -> int
     );
 
   const auto result_tiny_is_ok       = local::test_asinh(INT32_C(4096), false, 1.001, 1.1);
-  const auto result_small_is_ok      = local::test_asinh(INT32_C(96),   false, 0.1, 1.59);
+  const auto result_small_is_ok      = local::test_asinh(INT32_C(256),  false, 0.1, 1.59);
+  const auto result_small_neg_is_ok  = local::test_asinh(INT32_C(256),  true,  0.1, 1.59);
   const auto result_medium_is_ok     = local::test_asinh(INT32_C(48),   false, 1.59, 10.1);
   const auto result_medium_neg_is_ok = local::test_asinh(INT32_C(48),   true,  1.59, 10.1);
   const auto result_large_is_ok      = local::test_asinh(INT32_C(48),   false, 1.0E+01, 1.0E+19);
@@ -244,6 +245,7 @@ auto main() -> int
   BOOST_TEST(result_eps_is_ok);
   BOOST_TEST(result_tiny_is_ok);
   BOOST_TEST(result_small_is_ok);
+  BOOST_TEST(result_small_neg_is_ok);
   BOOST_TEST(result_medium_is_ok);
   BOOST_TEST(result_medium_neg_is_ok);
   BOOST_TEST(result_large_is_ok);
